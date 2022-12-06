5b53a184390f2e8d16d7a5400805d7ab444a2eb6
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2022-12-06 12:02:42 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+
+from copy import deepcopy
+
+f = open('input.txt')
+lst = f.read().split('\n')
+
+Break = lst.index('')
+stacks = {int(i): [] for i in lst[Break - 1] if i.isnumeric()}
+indexes = {num: lst[Break-1].index(str(num)) for num in stacks}
+
+for i, x in enumerate(lst):
+    if i < Break-1:
+        while len(x) < len(lst[Break-1]):
+            x += ' '
+        for stack, index in indexes.items():
+            if x[index] != ' ':
+                stacks[stack].append(x[index])
+    if i == Break:
+        stacks1 = deepcopy(stacks)
+        stacks2 = deepcopy(stacks)
+    elif i > Break:
+        value, start, end = [int(j) for j in x.split(' ') if j.isnumeric()]
+
+        # Part 1
+        for _ in range(value):
+            char = stacks1[start][0]
+            stacks1[start].pop(0)
+            stacks1[end].insert(0, char)
+
+        # Part 2
+        char = []
+        for _ in range(value):
+            char.append(stacks2[start][0])
+            stacks2[start].pop(0)
+        stacks2[end] = (stacks2[end][::-1] + char[::-1])[::-1]
+
+msg1 = ''
+for stack in stacks1:
+    msg1 += stacks1[stack][0]
+
+msg2 = ''
+for stack in stacks2:
+    msg2 += stacks2[stack][0]
+
+print(msg1, msg2)
