01153398afc857a02f882b60d70c87587b57ac2f
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2022-12-07 18:00:51 +0800

    Update main.py

--- Day 4/main.py
+++ Day 4/main.py
@@ -9,10 +9,12 @@ for x in lst:
     b1, b2 = [int(i) for i in x[1].split('-')]
     a = set(range(a1, a2 + 1))
     b = set(range(b1, b2 + 1))
-
+    
+    # Part 1
     if a.union(b) == a or a.union(b) == b:
         count1 += 1
-
+    
+    # Part 2
     if a.intersection(b):
         count2 += 1
 
