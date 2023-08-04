d3ee0657637da1e2467b1ada84e12eec39a295bb
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2023-08-04 23:48:54 +0800

    Create main.py

---
+++ 2022/Day 25/main.py
@@ -0,0 +1,15 @@
+from math import log, ceil
+
+with open('input2.txt', 'r') as f:
+    Input = [[{'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}[sym] for sym in list(x)] for x in f.read().split('\n')]
+    numbers = [sum([num * 5 ** idx for idx, num in enumerate(reversed(x))]) for x in Input]
+    total = sum(numbers)
+    total += int(''.join(('2' for i in range(ceil(log(total, 5))))), base=5)
+    SNAFU = ''
+    idx = int(log(total, 5))
+    while total > 0:
+        val = total // (5 ** idx)
+        SNAFU += {2: '2', 1: '1', 0: '0', -1: '-', -2: '='}[val - 2]
+        total -= val * (5 ** idx)
+        idx -= 1
+    print(SNAFU)
