4bc788d9f2a4e1231095eef4c40d47afb00cee9a
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2023-08-09 21:26:02 +0800

    Update main.py

--- 2021/Day 7/main.py
+++ 2021/Day 7/main.py
@@ -1,8 +1,8 @@
-f = open('input.txt')
-lst = [int(i) for i in f.read().split(',') if i.isnumeric()]
+with open('input.txt', 'r') as f:
+    lst = [int(i) for i in f.read().split(',')]
 
 total_fuel1 = {pos: sum([abs(x-pos) for x in lst]) for pos in lst}  # Part 1
 total_fuel2 = {pos: sum([int(abs(x-pos)*(abs(x-pos)+1)/2) for x in lst]) for pos in lst}  # Part 2
 
-print(min(total_fuel1.values()))
-print(min(total_fuel2.values()))
+print(f"Part 1: {min(total_fuel1.values())}")
+print(f"Part 2: {min(total_fuel2.values())}")
