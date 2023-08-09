03c6577af14ac96b3909e1e3439d1139481a21f3
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2023-08-09 21:19:29 +0800

    Update main.py

--- 2022/Day 7/main.py
+++ 2022/Day 7/main.py
@@ -1,5 +1,6 @@
-f = open('input.txt')
-lst = f.read().split('\n')
+
+with open('input.txt', 'r') as f:
+    lst = f.read().split('\n')
 
 directories = {}
 list_of_directories = []
@@ -11,11 +12,9 @@ for x in lst:
         if cd not in directories:
             directories[cd] = []
         list_of_directories.append(dir)
-
     elif '$ cd' in x and '..' in x:
         cd = cd.removesuffix(list_of_directories[len(list_of_directories)-1])
         list_of_directories.pop(len(list_of_directories)-1)
-
     elif '$ cd' not in x and '$ ls' not in x:
         if 'dir' in x:
             dir = x.removeprefix('dir ')
@@ -36,21 +35,16 @@ for key in total_size.keys():
         if key in directory and key != directory:
             total_size[key] += total_size[directory]
 
-# Part 1
 total = 0
-
-# Part 2
 empty_space = 70_000_000 - max(total_size.values())
 space_needed = 30_000_000 - empty_space
 temp = []
 
 for directory in total_size:
-    # Part 1
     if total_size[directory] <= 100_000:
         total += total_size[directory]
-    # Part 2
     if total_size[directory] >= space_needed:
         temp.append(total_size[directory])
 
-print(total)
-print(min(temp))
+print(f"Part 1: {total}")
+print(f"Part 2: {min(temp)}")
