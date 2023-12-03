551ecce6888d4196b48709bb54d4905b495c505e
kumar2215 <113902321+kumar2215@users.noreply.github.com>
2023-12-03 14:10:18 +0800

    Create main.py

---
+++ 2023/Day 3/main.py
@@ -0,0 +1,54 @@
+
+with open('input.txt') as f:
+    content = f.read().split('\n')
+    grid = {}
+    for j, line in enumerate(content, start=1):
+        for i, char in enumerate(line, start=1):
+            grid[(i, j)] = char
+
+nums = {}
+visited = []
+
+for pt in grid:
+    if grid[pt].isnumeric() and pt not in visited:
+        lst = [pt]
+        s = grid[pt]
+        visited.append(pt)
+        while True:
+            next_pt = (pt[0] + 1, pt[1])
+            if next_pt in grid and grid[next_pt].isnumeric():
+                lst.append(next_pt)
+                s += grid[next_pt]
+                visited.append(next_pt)
+                pt = next_pt
+            else:
+                nums[tuple(lst)] = s
+                break
+
+grid = {pt: grid[pt] for pt in grid if not grid[pt].isnumeric()}
+part_numbers = []
+gears = {}
+
+for points in nums:
+    surrounding = []
+    num = int(nums[points])
+    for point in points:
+        x, y = point
+        poss_surrounding = [(x + i, y + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if i != j or i != 0]
+        for pt in poss_surrounding:
+            if pt in grid and pt not in points:
+                surrounding.append(pt)
+    if any(grid[pt] != '.' for pt in surrounding):
+        part_numbers.append(num)
+    for pt in surrounding:
+        if pt in grid and grid[pt] == '*':
+            if pt in gears:
+                gears[pt].append(num)
+            else:
+                gears[pt] = [num]
+
+gears = {pt: list(set(gears[pt])) for pt in gears}
+gears = {pt: gears[pt][0] * gears[pt][1] for pt in gears if len(gears[pt]) == 2}
+
+print(f"Part 1: {sum(part_numbers)}")
+print(f"Part 2: {sum(gears.values())}")
