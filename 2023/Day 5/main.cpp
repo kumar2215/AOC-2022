7dd6c76ea9c64cc11de185f9a81e0f4f5258ea6c
Kumar <113902321+kumar2215@users.noreply.github.com>
2024-01-26 13:36:53 +0800

    Update main.cpp

--- 2023/Day 5/main.cpp
+++ 2023/Day 5/main.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 #include <vector>
 #include <string>
-#include <bits/stdc++.h>
 #include <algorithm>
-#include <memory>
-#include <map>
-#include <set>
 
 using namespace std;
 
 class range {
 public:
-    long long lower, upper;
+    int64_t lower, upper;
 
-    range(long long lower, long long upper) : lower(lower), upper(upper - 1) {}
+    range(int64_t lower, int64_t upper) : lower(lower), upper(upper - 1) {}
 
-    bool contains(long long x) {
+    bool contains(int64_t x) {
         return x >= this->lower && x <= this->upper;
     }
 };
 
 class Function {
 public:
-    map<shared_ptr<range>, long long> ranges;
+    vector<pair<range, int64_t>> ranges;
 
-    explicit Function (map<shared_ptr<range>, long long> ranges) {
-        this->ranges = ranges;
-    }
+    explicit Function (vector<pair<range, int64_t>> ranges) : ranges(ranges) {}
 
-    long long operator()(long long x) {
+    int64_t operator()(int64_t x) {
         for (auto &it : this->ranges) {
-            range R = *it.first;
-            if (R.contains(x)) {
+            if (it.first.contains(x)) {
                 return x + it.second;
             }
         }
         return x;
     }
 
-    long long inv(long long y) {
+    int64_t inv(int64_t y) {
         for (auto &it : this->ranges) {
-            range R = *it.first;
-            const long long c = it.second;
-            if (R.contains(y - c)) {
+            const int64_t c = it.second;
+            if (it.first.contains(y - c)) {
                 return y - c;
             }
         }
@@ -54,16 +47,16 @@ public:
 
 fstream Input;
 vector<string> lines;
-vector<long long> seeds;
-vector<shared_ptr<range>> seeds2;
+vector<int64_t> seeds;
+vector<range> seeds2;
 typedef pair<string, string> category;
-map<category, shared_ptr<Function>> mappings, mappings2;
+vector<pair<category, Function>> mappings, mappings2;
 
-long long minimum(const vector<long long>& nums) {
+int64_t minimum(const vector<int64_t>& nums) {
     return *min_element(nums.begin(), nums.end());
 }
 
-vector<string> split(string str, char separator) {
+vector<string> split(const string &str, char separator) {
     int startIndex = 0, endIndex = 0;
     vector<string> result;
     for (int i = 0; i <= str.size(); i++) {
@@ -93,13 +86,12 @@ void initialisation() {
     }
     category curr;
     vector<string> temp;
-    long long DR, SR, RL;
-    map<shared_ptr<range>, long long> ranges;
-    shared_ptr<range> r;
+    int64_t DR, SR, RL;
+    vector<pair<range, int64_t>> ranges;
     for (int i = 2; i < lines.size(); i++) {
         line = lines[i];
         if (line.find('-') != string::npos) {
-            temp = split(lines[i], '-');
+            temp = split(line, '-');
             s = temp[2];
             curr = {temp[0], s.replace(s.find(" map:"), 5, "")};
         } else if (!line.empty()) {
@@ -107,56 +99,53 @@ void initialisation() {
             DR = stoll(temp[0]);
             SR = stoll(temp[1]);
             RL = stoll(temp[2]);
-            r = make_shared<range>(SR, SR + RL);
-            ranges.insert({r, DR - SR});
+            range r {SR, SR + RL};
+            ranges.push_back({r, DR - SR});
         } else {
-            shared_ptr<Function> f = make_shared<Function>(ranges);
-            mappings[curr] = f;
+            mappings.push_back({curr, Function {ranges} });
             ranges.clear();
         }
     }
     for (auto &it : mappings) {
         curr = it.first;
-        mappings2.insert({{curr.second, curr.first}, it.second});
+        mappings2.push_back({{curr.second, curr.first}, it.second});
     }
     for (int i = 0; i < seeds.size() / 2; i++) {
         SR = seeds[2 * i]; RL = seeds[2 * i + 1];
-        r = make_shared<range>(SR, SR + RL);
+        range r = {SR, SR + RL};
         seeds2.push_back(r);
     }
 }
 
-long long inv(long long x) {
+int64_t inv(int64_t x) {
     string curr = "location";
     category cat;
-    shared_ptr<Function> f;
     while (curr != "seed") {
         for (auto &it: mappings2) {
-            cat = it.first; f = it.second;
-            if (cat.first == curr) {
+            if (it.first.first == curr) {
+                x = it.second.inv(x);
+                curr = it.first.second;
                 break;
             }
         }
-        x = (*f).inv(x);
-        curr = cat.second;
     }
     return x;
 }
 
-bool inside_seeds2(long long x) {
+bool inside_seeds2(int64_t x) {
     for (auto &it : seeds2) {
-        if ((*it).contains(x)) {
+        if (it.contains(x)) {
             return true;
         }
     }
     return false;
 }
 
-long long get_min_location(vector<long long> seeds) {
-    vector<long long> locations = seeds;
+int64_t get_min_location() {
+    vector<int64_t> locations = seeds;
     string curr = "seed";
     category cat;
-    shared_ptr<Function> f;
+    Function f {{}};
     while (curr != "location") {
         for (auto &it : mappings) {
             cat = it.first; f = it.second;
@@ -164,8 +153,8 @@ long long get_min_location(vector<long long> seeds) {
                 break;
             }
         }
-        for (int i = 0; i < locations.size(); i++) {
-            locations[i] = (*f)(locations[i]);
+        for (int64_t & location : locations) {
+            location = (f)(location);
         }
         curr = cat.second;
     }
@@ -174,10 +163,21 @@ long long get_min_location(vector<long long> seeds) {
 
 int main() {
     initialisation();
-    printf("Part 1: %llu\n", get_min_location(seeds));
-    long long m = 0;
-    while (!inside_seeds2(inv(m))) {
-        m++;
+    printf("Part 1: %llu\n", get_min_location());
+    int64_t m = 0;
+    int64_t LB = 0;
+    int64_t UB = 1;
+    while (!inside_seeds2(inv(UB))) {
+        UB *= 2;
+    }
+    while (LB != UB) {
+        m = LB + (UB - LB) / 2;
+        if (inside_seeds2(inv(m))) {
+            UB = m;
+        }
+        else {
+            LB = m + 1;
+        }
     }
     printf("Part 2: %llu", m);
     return 0;
