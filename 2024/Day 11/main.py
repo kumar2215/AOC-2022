90f8c6fba64911961753b85f575ff38f3e55739c
kumar2215 <kumar@LAPTOP-MR00U8RR>
2024-12-11 18:11:10 +0800

    Fixed Day 11 Python

--- 2024/Day 11/main.py
+++ 2024/Day 11/main.py
@@ -4,10 +4,11 @@ with open("input.txt") as f:
     org_stones = [int(x) for x in f.read().split(" ")]
 
 PART1, BLINKS = 24, 75
-total = Value('i', 0)
-total2 = Value('i', 0)
+total = Value('l', 0)  
+total2 = Value('l', 0)
 
 def handle_stone(num, blinks):
+    global total, total2
     stones = Counter([num])
     for i in range(blinks):
         new_stones = Counter()
@@ -15,11 +16,15 @@ def handle_stone(num, blinks):
             if stone == 0: new_stones[1] += stones[stone] 
             elif len(str(stone)) % 2 == 0:
                 mid = len(str(stone)) // 2
-                new_stones.update([int(str(stone)[:mid]), int(str(stone)[mid:])] * stones[stone])
+                part1 = int(str(stone)[:mid])
+                part2 = int(str(stone)[mid:])
+                new_stones[part1] += stones[stone]
+                new_stones[part2] += stones[stone]
             else:
-                new_stones.update([stone * 2024] * stones[stone])
+                new_stones[stone * 2024] += stones[stone]
         stones = new_stones
-        if i == PART1: total.value += sum(stones.values())
+        if i == PART1: 
+            total.value += sum(stones.values())
     total2.value += sum(stones.values())
         
 processes = []
